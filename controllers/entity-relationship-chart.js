7c8c9b45a24edec104b0fddfddfdea08200ecdd8
romanmandryk <roman.mandryk@gmail.com>
2020-06-13 21:40:55 +1000

    added users-permissions models

--- controllers/entity-relationship-chart.js
+++ controllers/entity-relationship-chart.js
@@ -6,6 +6,8 @@
  * @description: A set of functions called "actions" of the `entity-relationship-chart` plugin.
  */
 
+const userPermissionModels = ['User', 'Role', 'Permission']
+
 module.exports = {
 
   /**
@@ -26,7 +28,11 @@ module.exports = {
     Object.keys(models).forEach(modelKey => {
       const model = models[modelKey];
       data.push({name: model.info.name, attributes: model.attributes, key:modelKey});
-    })
+    });
+    userPermissionModels.forEach(name => {
+      const model = strapi.query(name, 'users-permissions').model;
+      data.push({name: name, attributes: model.attributes, key:model.info.name});
+    });
     ctx.send({
       data
     });
