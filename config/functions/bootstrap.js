fd1ee87fe3ee3fb5e35498352be548d1e1545c4a
romanmandryk <roman.mandryk@gmail.com>
2020-06-13 21:39:43 +1000

    added permissions for auth role

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,1 +1,12 @@
-module.exports = async () => {}
+
+async function enableAuthPermissionForERData(){
+  const roleAuthenticated = await strapi.query('Role', 'users-permissions').findOne({ name: 'Authenticated' });
+  let permission = roleAuthenticated.permissions.find(p => p.controller === 'entity-relationship-chart' && p.action === 'geterdata');
+  if (permission && !permission.enabled){
+    await strapi.query('Permission', 'users-permissions').update({ id: permission.id }, { enabled: true });
+  }
+}
+
+module.exports = async () => {
+  await enableAuthPermissionForERData();
+}
