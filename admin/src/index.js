3b0cf7b56ddf33651ab8f2f4b5f3b485e28577a9
romanmandryk <roman.mandryk@gmail.com>
2022-01-08 14:08:18 +0000

    renamed and added custom menu label

--- admin/src/index.js
+++ admin/src/index.js
@@ -13,7 +13,7 @@ export default {
       icon: PluginIcon,
       intlLabel: {
         id: `${pluginId}.plugin.name`,
-        defaultMessage: name,
+        defaultMessage: 'ER Chart',
       },
       Component: async () => {
         const component = await import(/* webpackChunkName: "[request]" */ './pages/App');
